25e4ff72641cf0980f9fa7362b1b1ba4edb029dd
Simonov Denis <sim-mail@list.ru>
2024-08-13 22:31:08 +0300

    .

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 Создание интерфейсов для языка C.
 
 ```bash
-cloop FirebirdInterface.idl c-header fb_api.h FB_OOAPI_H fb_
-cloop FirebirdInterface.idl c-impl fb_api_impl.c fb_api.h fb_
+cloop FirebirdInterface.idl c-header fb_c_api.h FB_C_API_H I
+cloop FirebirdInterface.idl c-impl fb_c_api.c fb_c_api.h I
 ```
 
-В получившихся файлах не хватает функции `fb_get_master_interface()`. Добавим её определение в файл `fb_api.h`.
+В получившихся файлах не хватает функции `fb_get_master_interface()`. Добавим её определение в файл `fb_c_api.h`.
 
 ```cpp
 CLOOP_EXTERN_C struct fb_Master* ISC_EXPORT fb_get_master_interface(void);
--- example.c
+++ example.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "fb_api.h"
+#include "fb_c_api.h"
 
 typedef struct fb_field
 {
@@ -14,14 +14,14 @@ typedef struct fb_field
 } fb_field_t;
 
 
-void fill_field(fb_field_t* field, struct fb_MessageMetadata* meta, struct fb_Status* status, unsigned field_no)
+void fill_field(fb_field_t* field, struct IMessageMetadata* meta, struct IStatus* status, unsigned field_no)
 {
 	memset(field, 0, sizeof(fb_field_t));
-	field->field_type = fb_MessageMetadata_getType(meta, status, field_no);
-	field->offset = fb_MessageMetadata_getOffset(meta, status, field_no);
-	field->nullOffset = fb_MessageMetadata_getNullOffset(meta, status, field_no);
-	field->scale = fb_MessageMetadata_getScale(meta, status, field_no);	
-	const char* name  = fb_MessageMetadata_getAlias(meta, status, field_no);
+	field->field_type = IMessageMetadata_getType(meta, status, field_no);
+	field->offset = IMessageMetadata_getOffset(meta, status, field_no);
+	field->nullOffset = IMessageMetadata_getNullOffset(meta, status, field_no);
+	field->scale = IMessageMetadata_getScale(meta, status, field_no);	
+	const char* name  = IMessageMetadata_getAlias(meta, status, field_no);
 	
 	memcpy(field->name, name, 252);
 }
@@ -40,100 +40,105 @@ int main (int argc, char** argv)
 {
 	printf("C API test\n");
 
-	struct fb_Master* master = fb_get_master_interface();
-	struct fb_Status* status = fb_Master_getStatus(master);
-	struct fb_Provider* provider = fb_Master_getDispatcher(master);
-	struct fb_Util* util = fb_Master_getUtilInterface(master);
-	struct fb_Int128* int128u = NULL;
+	struct IMaster* master = fb_get_master_interface();
+	struct IStatus* status = IMaster_getStatus(master);
+	struct IProvider* provider = IMaster_getDispatcher(master);
+	struct IUtil* util = IMaster_getUtilInterface(master);
+	struct IInt128* int128u = NULL;
 
-	struct fb_XpbBuilder* dpb_builder = NULL;
+	struct IXpbBuilder* dpb_builder = NULL;
 	unsigned dpb_length = 0;
 	const unsigned char* dpb = NULL;
 
-	struct fb_Attachment* att = NULL;
+	struct IAttachment* att = NULL;
 
-	struct fb_XpbBuilder* tpb_builder = NULL;
+	struct IXpbBuilder* tpb_builder = NULL;
 	unsigned tpb_length = 0;
 	const unsigned char* tpb = NULL;
 
-	struct fb_Transaction* tra = NULL;
+	struct ITransaction* tra = NULL;
 
-	struct fb_Statement* stmt = NULL;
+	struct IStatement* stmt = NULL;
 
-	struct fb_MessageMetadata* meta = NULL;
+	struct IMessageMetadata* meta = NULL;
 
 	unsigned column_count = 0;
 	unsigned cursor_data_size = 0;
 
-	struct fb_ResultSet* rs = NULL;
+	struct IResultSet* rs = NULL;
 
 	unsigned char* cursor_data = NULL;
+    unsigned client_version = 0;
 
 	do {
-		int128u = fb_Util_getInt128(util, status);
+        client_version = IUtil_getClientVersion(util);
+        printf("Client Version: %d; major: %d, minor: %d", client_version, (client_version >> 8) & 0xFF, client_version & 0xFF);
+        printf("\n");
+
+		int128u = IUtil_getInt128(util, status);
 		/* check error */
-		if (fb_Status_getState(status)) {
+		if (IStatus_getState(status)) {
 			break;
 		}
 
 		/* build database parameters buffer */
-		dpb_builder = fb_Util_getXpbBuilder(util, status, fb_XpbBuilder_DPB, NULL, 0);
-		fb_XpbBuilder_insertString(dpb_builder, status, isc_dpb_user_name, db_user);
-		fb_XpbBuilder_insertString(dpb_builder, status, isc_dpb_password, db_password);
-		dpb_length = fb_XpbBuilder_getBufferLength(dpb_builder, status);
-		dpb = fb_XpbBuilder_getBuffer(dpb_builder, status);
+		dpb_builder = IUtil_getXpbBuilder(util, status, IXpbBuilder_DPB, NULL, 0);
+		IXpbBuilder_insertString(dpb_builder, status, isc_dpb_user_name, db_user);
+		IXpbBuilder_insertString(dpb_builder, status, isc_dpb_password, db_password);
+		dpb_length = IXpbBuilder_getBufferLength(dpb_builder, status);
+		dpb = IXpbBuilder_getBuffer(dpb_builder, status);
 		/* check error */
-		if (fb_Status_getState(status)) {
+		if (IStatus_getState(status)) {
 			break;
 		}
 
 		printf("Attache to database...\n");
-		att = fb_Provider_attachDatabase(provider, status, db_uri, dpb_length, dpb);
+		att = IProvider_attachDatabase(provider, status, db_uri, dpb_length, dpb);
 		/* check error */
-		if (fb_Status_getState(status)) {
+		if (IStatus_getState(status)) {
 			break;
 		}
 
 		/* build transaction parameters buffer */
-		tpb_builder = fb_Util_getXpbBuilder(util, status, fb_XpbBuilder_TPB, NULL, 0);
-		fb_XpbBuilder_insertTag(tpb_builder, status, isc_tpb_read_committed);
-		fb_XpbBuilder_insertTag(tpb_builder, status, isc_tpb_read_consistency);
-		fb_XpbBuilder_insertTag(tpb_builder, status, isc_tpb_wait);
-		fb_XpbBuilder_insertTag(tpb_builder, status, isc_tpb_read);
-		tpb_length = fb_XpbBuilder_getBufferLength(tpb_builder, status);
-		tpb = fb_XpbBuilder_getBuffer(tpb_builder, status);
+		tpb_builder = IUtil_getXpbBuilder(util, status, IXpbBuilder_TPB, NULL, 0);
+		IXpbBuilder_insertTag(tpb_builder, status, isc_tpb_read_committed);
+		IXpbBuilder_insertTag(tpb_builder, status, isc_tpb_read_consistency);
+		IXpbBuilder_insertTag(tpb_builder, status, isc_tpb_wait);
+		IXpbBuilder_insertTag(tpb_builder, status, isc_tpb_read);
+		tpb_length = IXpbBuilder_getBufferLength(tpb_builder, status);
+		tpb = IXpbBuilder_getBuffer(tpb_builder, status);
 		/* check error */
-		if (fb_Status_getState(status)) {
+		if (IStatus_getState(status)) {
 			break;
 		}
 
 		printf("Start transaction...\n");
-		tra = fb_Attachment_startTransaction(att, status, tpb_length, tpb);
+		tra = IAttachment_startTransaction(att, status, tpb_length, tpb);
 		/* check error */
-		if (fb_Status_getState(status)) {
+		if (IStatus_getState(status)) {
 			break;
 		}
 
 		printf("Prepare statement...\n");
-		stmt = fb_Attachment_prepare(att, status, tra, 0, cursor_sql, 3, fb_Statement_PREPARE_PREFETCH_METADATA);
+		stmt = IAttachment_prepare(att, status, tra, 0, cursor_sql, 3, IStatement_PREPARE_PREFETCH_METADATA);
 		/* check error */
-		if (fb_Status_getState(status)) {
+		if (IStatus_getState(status)) {
 			break;
 		}
 
 		/* get metadata */
-		meta = fb_Statement_getOutputMetadata(stmt, status);
-		column_count = fb_MessageMetadata_getCount(meta, status);
-		cursor_data_size = fb_MessageMetadata_getMessageLength(meta, status);
+		meta = IStatement_getOutputMetadata(stmt, status);
+		column_count = IMessageMetadata_getCount(meta, status);
+		cursor_data_size = IMessageMetadata_getMessageLength(meta, status);
 		/* check error */
-		if (fb_Status_getState(status)) {
+		if (IStatus_getState(status)) {
 			break;
 		}
 
 		printf("Open cursor...\n");
-		rs = fb_Statement_openCursor(stmt, status, tra, NULL, NULL, meta, 0);
+		rs = IStatement_openCursor(stmt, status, tra, NULL, NULL, meta, 0);
 		/* check error */
-		if (fb_Status_getState(status)) {
+		if (IStatus_getState(status)) {
 			break;
 		}
 
@@ -143,9 +148,9 @@ int main (int argc, char** argv)
 			break;
 		}
 
-		while (fb_ResultSet_fetchNext(rs, status, cursor_data) == fb_Status_RESULT_OK) {
+		while (IResultSet_fetchNext(rs, status, cursor_data) == IStatus_RESULT_OK) {
 			/* check error */
-			if (fb_Status_getState(status)) {
+			if (IStatus_getState(status)) {
 				break;
 			}
 			short nullFlag;
@@ -157,7 +162,7 @@ int main (int argc, char** argv)
 			for (unsigned i = 0; i < column_count; ++i) {
                 fb_field_t field = {0};
 				fill_field(&field, meta, status, i);
-                if (fb_Status_getState(status)) {
+                if (IStatus_getState(status)) {
 					goto cleanup;
 				}
 
@@ -172,8 +177,8 @@ int main (int argc, char** argv)
 				case SQL_INT128:
 				{
 					i128 = (FB_I128*)(cursor_data + field.offset);
-					char int128Buf[fb_Int128_STRING_SIZE] = {0};
-					fb_Int128_toString(int128u, status, i128, field.scale, fb_Int128_STRING_SIZE, int128Buf);
+					char int128Buf[IInt128_STRING_SIZE] = {0};
+					IInt128_toString(int128u, status, i128, field.scale, IInt128_STRING_SIZE, int128Buf);
 					printf("%s", int128Buf);
 				}
 					break;
@@ -181,7 +186,7 @@ int main (int argc, char** argv)
 				{
 					ts = (ISC_TIMESTAMP_TZ*)(cursor_data + field.offset);
 					char tz_buffer[60] = { 0 };
-					fb_Util_decodeTimeStampTz(util, status, ts,
+					IUtil_decodeTimeStampTz(util, status, ts,
 						&year, &month, &day,
 						&hour, &minute, &second, &fraction,
 						sizeof(tz_buffer), tz_buffer);
@@ -194,7 +199,7 @@ int main (int argc, char** argv)
 				}
 
 				/* check error */
-				if (fb_Status_getState(status)) {
+				if (IStatus_getState(status)) {
 					goto cleanup;
 				}
 
@@ -204,42 +209,42 @@ int main (int argc, char** argv)
 		}
 
 		/* check error */
-		if (fb_Status_getState(status)) {
+		if (IStatus_getState(status)) {
 			break;
 		}
 
 		printf("Close cursor...\n");
-		fb_ResultSet_close(rs, status);
+		IResultSet_close(rs, status);
 		/* check error */
-		if (fb_Status_getState(status)) {
+		if (IStatus_getState(status)) {
 			break;
 		}
 		rs = NULL;
 
 		/* release metadata */
-		fb_MessageMetadata_release(meta);
+		IMessageMetadata_release(meta);
 		meta = NULL;
 
 		/* Free statement */
-		fb_Statement_free(stmt, status);
+		IStatement_free(stmt, status);
 		/* check error */
-		if (fb_Status_getState(status)) {
+		if (IStatus_getState(status)) {
 			break;
 		}
 		stmt = NULL;
 
 		printf("Commit transaction...\n");
-		fb_Transaction_commit(tra, status);
+		ITransaction_commit(tra, status);
 		/* check error */
-		if (fb_Status_getState(status)) {
+		if (IStatus_getState(status)) {
 			break;
 		}
 		tra = NULL;
 
 		printf("Detach from database...\n");
-		fb_Attachment_detach(att, status);
+		IAttachment_detach(att, status);
 		/* check error */
-		if (fb_Status_getState(status)) {
+		if (IStatus_getState(status)) {
 			break;
 		}
 		att = NULL;
@@ -253,38 +258,38 @@ cleanup:
 	free(cursor_data);
 	cursor_data = NULL;
 	if (rs) {
-		fb_ResultSet_release(rs);
+		IResultSet_release(rs);
 		rs = NULL;
 	}
 	if (meta) {
-		fb_MessageMetadata_release(meta);
+		IMessageMetadata_release(meta);
 		meta = NULL;
 	}
 	if (stmt) {
-		fb_Statement_release(stmt);
+		IStatement_release(stmt);
 		stmt = NULL;
 	}
 	if (tra) {
-		fb_Transaction_release(tra);
+		ITransaction_release(tra);
 		tra = NULL;
 	}
 	if (tpb_builder) {
-		fb_XpbBuilder_dispose(tpb_builder);
+		IXpbBuilder_dispose(tpb_builder);
 		tpb_builder = NULL;
 	}
 	if (att) {
-		fb_Attachment_release(att);
+		IAttachment_release(att);
 		att = NULL;
 	}
 	if (dpb_builder) {
-		fb_XpbBuilder_dispose(dpb_builder);
+		IXpbBuilder_dispose(dpb_builder);
 		dpb_builder = NULL;
 	}
 
 	/* check error and print message */
-	if (fb_Status_getState(status)) {
+	if (IStatus_getState(status)) {
 		char buf[256];
-		fb_Util_formatStatus(util, buf, sizeof(buf), status);
+		IUtil_formatStatus(util, buf, sizeof(buf), status);
 		printf("Error: %s\n", buf);
 		return 1;
 	}
